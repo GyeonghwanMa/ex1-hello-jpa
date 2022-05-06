7ae25d60c16b232352a3713021f91bf3fafa0601
Gyeonghwan <ghma1213@gmail.com>
2022-05-06 11:38:15 +0900

    엔티티 매핑 = 필드와 컬럼 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -12,23 +12,6 @@ public class Member {
     @Column(name="name")
     private String username;
 
-    private  Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
-    @Transient
-    private int temp;
-
     public Member() {
     }
 
